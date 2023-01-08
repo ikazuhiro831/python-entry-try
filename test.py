ea84e78e3d74c5dbfc3f1136aa99748a728c4e55
kazu <i_kazuhiro831@yahoo.co.jp>
2023-01-08 11:57:26 +0900

    判定処理追加

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 def hantei(arg):
   if arg > 70 :
-    print("合格")
+    print("合格" + str(arg))
   else :
-    print("不合格")
+    print("不合格" + str(arg))
 
-hantei(71)
+# hantei(70)
+
+score = 0
+
+for index in range(1,101):
+  score = score + 1
+  hantei(score)
+   
