f40351f05b4729fd6f9aa76d60ac4b685580f74e
kazu <i_kazuhiro831@yahoo.co.jp>
2023-01-08 21:55:17 +0900

    classを利用

--- test.py
+++ test.py
@@ -12,5 +12,26 @@
 #   score = score + 1
 #   hantei(score)
 
-with open('./test.txt', 'r') as file:
-  print(file.read())
+# with open('./test.txt', 'r') as file:
+#   print(file.read())
+
+class Card:
+  def __init__(self, date ,user_name):  #メソッド     classの中では関数ではない
+    self.date = date                    #プロパティ
+    self.user_name = user_name
+  def message(self):
+    return 'この投稿は' + self.user_name + 'が' + self.date + 'に投稿しました'
+
+date_a = '2023-01-08'
+user_name_a = 'Taro'
+card_a = Card(date_a, user_name_a)
+
+date_b = '2023-01-11'
+user_name_b = 'Piroko'
+card_b = Card(date_b, user_name_b)   #インスタンス化
+
+# print(card_a.user_name)
+# print(card_a.date)
+# print(card_a.message())
+
+print(card_b.message())
