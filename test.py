800f7446e66c994fd7510e887c18ac789db48555
kazu <i_kazuhiro831@yahoo.co.jp>
2023-01-08 21:35:37 +0900

    withとopen関数を利用

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-def hantei(arg):
-  if arg > 60 :
-    print("合格" + str(arg))
-  else :
-    print("不合格" + str(arg))
+# def hantei(arg):
+#   if arg > 60 :
+#     print("合格" + str(arg))
+#   else :
+#     print("不合格" + str(arg))
 
-# hantei(70)
+# # hantei(70)
 
-score = 0
+# score = 0
 
-for index in range(1,101):
-  score = score + 1
-  hantei(score)
+# for index in range(1,101):
+#   score = score + 1
+#   hantei(score)
+
+with open('./test.txt', 'r') as file:
+  print(file.read())
